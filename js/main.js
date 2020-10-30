dc86c37b989ae076c68fdc8d8cc12482c8fe3a6c
Alexandre Xavier <51323512+Allexandrexavier@users.noreply.github.com>
2020-10-30 16:58:28 -0300

    alteracoes e testes de funcoes

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ while (count < 5) {
   console.log(count);
   count = count + 1;
 } */
-
+/*
 var count;
 for (count = 0; count < 5; count++) {
   console.log(count);
@@ -56,9 +56,37 @@ function valida(idade) {
     console.log('maior idade');
   } else {
     console.log('menor idade');
-    return idade;
+    return valida;
   }
 }
-
-let idade = prompt('qual a sua idade');
+*/
+/* let idade = prompt('qual a sua idade');
 console.log(valida(idade));
+*/
+
+/*function botao() {
+  alert('obrigado');
+}
+*/
+
+function botao() {
+  document.getElementById('agd').innerHTML = 'obrigado por clicar';
+}
+
+function redirecionar() {
+  window.open(
+    'https://developer.mozilla.org/pt-BR/docs/Web/API/Element/innerHTML'
+  );
+}
+
+function mouse(elemento) {
+  elemento.innerHTML = 'obrigado por passar o mouse.';
+  //document.getElementById('onmouseover').innerHTML =
+  // 'obrigado por passar o mouse.';
+  // alert('obrigado');
+}
+
+function voltar(elemento) {
+  elemento.innerHTML = 'passe o mouse aqui.';
+  // document.getElementById('onmouseover').innerHTML = 'passe o mouse aqui.';
+}
